cc995b1fb55746e362d95115fee9251081702ad1
János Brodbeck <janos.brodbeck@posteo.de>
2019-06-02 02:27:59 +0200

    user type (leader, member) added to definitions

--- src/header/definitions.h
+++ src/header/definitions.h
@@ -8,8 +8,12 @@ extern "C" {
 #include <stdio.h>
 
 extern uint8_t       _cmd_counter;
+extern uint8_t       _user_type;
 
 #define _own_id =         {0x01};
+
+#define B2B_TYPE_USER           (0x0)
+#define B2B_TYPE_LEADEr         (0x1)
 
 #define B2B_CMD_SYNC_LEADER     (0x00)
 #define B2B_CMD_SYNC_MEMBER     (0x01)
